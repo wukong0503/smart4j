9cb563ebc05841916209ec231014d59e06505e83
wukong <wukong0503@gmail.com>
2017-06-27 23:17:16 +0800

    init

---
+++ chapter1/src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,31 @@
+package org.smart4j.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author lzming
+ * @since 2017-06-27.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        request.setAttribute("currentTime", currentTime);
+        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ chapter1/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lzming
+  Date: 2017/6/27
+  Time: 下午6:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>Hello!</h1>
+    <h2>当前时间: ${currentTime}</h2>
+</body>
+</html>
