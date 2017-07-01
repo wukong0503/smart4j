1761850a4b1d8ecfdc271b6f0d5e233119c601ea
wukong <wukong0503@gmail.com>
2017-07-01 20:26:51 +0800

    add: 完善服务层(2/3)

---
+++ chapter2/src/main/java/org/smart4j/chapter2/helper/DatabaseHelper.java
@@ -0,0 +1,173 @@
+package org.smart4j.chapter2.helper;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smart4j.chapter2.util.PropsUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @author wukong
+ * @since 2017-07-01.
+ */
+public class DatabaseHelper {
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseHelper.class);
+
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+
+    private static final QueryRunner QUERY_RUNNER = new QueryRunner();
+
+    // 为了确保一个线程中只有一个Connection，使用ThreadLocal来存放本地线程变量
+    private static final ThreadLocal<Connection> CONNECTION_HOLDER = new ThreadLocal<>();
+
+    static {
+        Properties conf = PropsUtil.loadProps("config.properties");
+        DRIVER = conf.getProperty("jdbc.driver");
+        URL = conf.getProperty("jdbc.url");
+        USERNAME = conf.getProperty("jdbc.username");
+        PASSWORD = conf.getProperty("jdbc.password");
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            logger.error("can not load jdbc driver", e);
+        } catch (Exception ex) {
+            logger.error("other exception", ex);
+        }
+    }
+
+    /**
+     * 获取数据库连接
+     */
+    public static Connection getConnection() {
+        Connection conn = CONNECTION_HOLDER.get();
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                logger.error("get connection failure", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_HOLDER.set(conn);
+            }
+        }
+        return conn;
+    }
+
+    /**
+     * 关闭数据库连接
+     */
+    public static void closeConnection() {
+        Connection conn = CONNECTION_HOLDER.get();
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                logger.error("close connection failure", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_HOLDER.remove();
+            }
+        }
+    }
+
+    /**
+     * 查询实体类列表
+     */
+    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params) {
+        List<T> entityList;
+        try {
+            Connection conn = getConnection();
+            entityList = QUERY_RUNNER.query(conn, sql, new BeanListHandler<T>(entityClass), params);
+        } catch (SQLException e) {
+            logger.error("query entity list failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return entityList;
+    }
+
+
+    /**
+     * 查询实体
+     */
+    public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params) {
+        T entity;
+        try {
+            Connection conn = getConnection();
+            entity = QUERY_RUNNER.query(conn, sql, new BeanHandler<T>(entityClass), params);
+        } catch (SQLException e) {
+            logger.error("query entity failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return entity;
+    }
+
+
+    /**
+     * 执行查询语句
+     */
+    public static List<Map<String, Object>> executeQuery(String sql, Object... params) {
+        List<Map<String, Object>> result;
+        try {
+            Connection conn = getConnection();
+            result = QUERY_RUNNER.query(conn, sql, new MapListHandler(), params);
+        } catch (SQLException e) {
+            logger.error("execute query failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return result;
+    }
+
+
+    /**
+     * 执行更新语句（包括update、insert、delete）
+     * @return rows 返回执行后影响的行数
+     */
+    public static int executeUpdate(String sql, Object... params) {
+        int rows = 0;
+        try {
+            Connection conn = getConnection();
+            rows = QUERY_RUNNER.update(conn, sql, params);
+        } catch (SQLException e) {
+            logger.error("execute update failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return rows;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -2,14 +2,11 @@ package org.smart4j.chapter2.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smart4j.chapter2.helper.DatabaseHelper;
 import org.smart4j.chapter2.model.Customer;
-import org.smart4j.chapter2.util.PropsUtil;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 /**
  * 提供客户数据服务
@@ -19,64 +16,13 @@ import java.util.Properties;
 public class CustomerService {
     private static Logger logger = LoggerFactory.getLogger(CustomerService.class);
 
-    private static final String DRIVER;
-    private static final String URL;
-    private static final String USERNAME;
-    private static final String PASSWORD;
-
-    static {
-        Properties conf = PropsUtil.loadProps("config.properties");
-        DRIVER = conf.getProperty("jdbc.driver");
-        URL = conf.getProperty("jdbc.url");
-        USERNAME = conf.getProperty("jdbc.username");
-        PASSWORD = conf.getProperty("jdbc.password");
-        try {
-            Class.forName(DRIVER);
-        } catch (ClassNotFoundException e) {
-            logger.error("can not load jdbc driver", e);
-        } catch (Exception ex) {
-            logger.error("other exception", ex);
-        }
-    }
-
-    public CustomerService() {
-    }
-
     /**
      * 获取客户列表
      * @return
      */
     public List<Customer> getCustomerList() {
-        Connection conn = null;
-        List<Customer> customerList = new ArrayList<>();
-        try {
-            String sql = "SELECT * FROM customer";
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                Customer customer = new Customer();
-                customer.setId(rs.getLong("id"));
-                customer.setName(rs.getString("name"));
-                customer.setContact(rs.getString("contact"));
-                customer.setTelephone(rs.getString("telephone"));
-                customer.setEmail(rs.getString("email"));
-                customer.setRemark(rs.getString("remark"));
-                customerList.add(customer);
-            }
-            // return customerList;
-        } catch (SQLException e) {
-            logger.error("execute sql failure", e);
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    logger.error("close connection failure", e);
-                }
-            }
-        }
-        return customerList;
+        String sql = "SELECT * FROM customer";
+        return DatabaseHelper.queryEntityList(Customer.class, sql);
     }
 
     public Customer getCustomer(long id) {
