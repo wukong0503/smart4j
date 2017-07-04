c4444c8bf947683dc242ebe2d6e98b1e38f84c4a
wukong <wukong0503@gmail.com>
2017-07-04 13:49:15 +0800

    完善控制器层

--- chapter2/src/main/java/org/smart4j/chapter2/controller/CustomerServlet.java
+++ chapter2/src/main/java/org/smart4j/chapter2/controller/CustomerServlet.java
@@ -1,11 +1,15 @@
 package org.smart4j.chapter2.controller;
 
+import org.smart4j.chapter2.model.Customer;
+import org.smart4j.chapter2.service.CustomerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @author wukong
@@ -13,9 +17,19 @@ import java.io.IOException;
  */
 @WebServlet(name = "customer")
 public class CustomerServlet extends HttpServlet {
+
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req, resp);
     }
 
     @Override
